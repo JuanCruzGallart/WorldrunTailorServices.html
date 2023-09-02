eaa9a7901b905aa7c2be94ce9bc5282df870c06f
JuanCruzGallart <gallartjuancruz@gmail.com>
2023-09-01 23:01:06 -0300

    EPIC TRADUCCION

--- js/script.js
+++ js/script.js
@@ -1,45 +1,106 @@
-const arrayIndexEsp = [];
-
-const landingIndexEsp = {
-    h1: `<h1>Soluciones hechas a medida para su Negocio</h1>`,
-    p: `<p>Consultoría sobre Negocios Internacionales para la economía global</p>`,
-    small: `<small>Nos encargamos de sus logísticas para que se pueda concentrar en lo importante.</small>`,
-    getStartedBtn: `<button>Comenzar Ahora</button>`,
-    learnMoreBtn: `<button>Aprender Más</button>`,
-    h2_n1: `<h2>Optimice las logísticas de su Negocio</h2>`,
-    p_n1: `<p>Identificamos las debilidades de su Negocio y las mejoramos por usted</p>`,
-    h2_n2:`<h2>Somos expertos en lo que hacemos</h2>`,
-    p_n2:`<p>Con más de 25 años de experiencia en el rubro, en Worldrun Tailor Services nos aseguramos de proveer las soluciones que su Negocio necesita.</p>`
+const indexEsp = {
+    h1: `Soluciones hechas a medida para su Negocio`,
+    p: `Consultoría sobre Negocios Internacionales para la economía global`,
+    small: `Nos encargamos de sus logísticas para que se pueda concentrar en lo importante.`,
+    h2_n1: `Optimice las logísticas de su Negocio`,
+    p_n1: `Identificamos las debilidades de su Negocio y las mejoramos por usted`,
+    h2_n2:`Somos expertos en lo que hacemos`,
+    p_n2:`Con más de 25 años de experiencia en el rubro, en Worldrun Tailor Services nos aseguramos de proveer las soluciones que su Negocio necesita.`
 };
 
-let espBtn = document.getElementById("esp_btn");
-let engBtn = document.getElementById("eng_btn");
+const navEsp = {
+    aHome: `Inicio`,
+    aServices: `Servicios`,
+    aContact: `Contacto`,
+    aAbout: `Nosotros`
+};
 
-//Landing Page variables
+const botonesEsp = {
+    getStarted: `Comenzar`,
+    learnMore: `Aprende Más`
+};
+
+const footerEsp = {
+    footerP: `Asesoría sobre Negocios Internacionales para la Economía Global. <br> Ante cualquier consulta o si desea adquirir nuestros servicios, llámenos.`,
+
+};
+
+//Nav variables
+let aHome = document.getElementsByClassName("a_home");
+let aServices = document.getElementsByClassName("a_services");
+let aContact = document.getElementsByClassName("a_contact");
+let aAbout = document.getElementsByClassName("a_about");
+
+//Button variables
+let getStartedBtn = document.getElementsByClassName("get-started-btn");
+let learnMoreBtn = document.getElementsByClassName("learn-more-btn");
+console.log(learnMoreBtn);
+
+//Footer variables
+let pFooter = document.getElementById("footer_p");
+
+//Index variables
 let h1Land = document.getElementById("index_landing_h1");
 let pLand = document.getElementById("index_landing_p");
 let smallLand = document.getElementById("index_landing_small"); 
-let getStartedBtn = document.getElementsByClassName("get-started-btn");
-let learnMoreBtn = document.getElementsByClassName("learn-more-btn");
 let h2_n1 = document.getElementById("h2_n1");
 let p_n1 = document.getElementById("p_n1");
 let h2_n2 = document.getElementById("h2_n2");
 let p_n2 = document.getElementById("p_n2");
 
-let lang = null;
 
-espBtn.addEventListener("click", ()=>{
-    h1Land.innerHTML = landingIndexEsp["h1"];
-    pLand.innerHTML = landingIndexEsp["p"];
-    smallLand.innerHTML = landingIndexEsp["small"];
-    getStartedBtn.innerHTML = landingIndexEsp["getStartedBtn"];
-    learnMoreBtn.innerHTML = landingIndexEsp["learnMoreBtn"];
-    h2_n1.innerHTML = landingIndexEsp["h2_n1"];
-    p_n1.innerHTML = landingIndexEsp["p_n1"];
-    h2_n2.innerHTML = landingIndexEsp["h2_n2"];
-    p_n2.innerHTML = landingIndexEsp["p_n2"];
+
+function changeLang(){
+    if(lang == "eng"){
+        lang = null;
+        location.reload();
+    }else if(lang == "esp"){
+
+        for (let i = 0; i < aHome.length; i++) {
+            aHome[i].innerHTML = navEsp["aHome"]
+          };
+        aServices.innerHTML = navEsp["aServices"];
+        aContact.innerHTML = navEsp["aContact"];
+        aAbout.innerHTML = navEsp["aAbout"];
+        h1Land.innerHTML = indexEsp["h1"];
+        pLand.innerHTML = indexEsp["p"];
+        smallLand.innerHTML = indexEsp["small"];
+        
+        for (let i = 0; i < getStartedBtn.length; i++) {
+            getStartedBtn[i].innerHTML = botonesEsp["getStarted"];
+          };
+        
+        for (let i = 0; i < learnMoreBtn.length; i++) {
+            learnMoreBtn[i].innerHTML = botonesEsp["learnMore"];
+          };
+
+        h2_n1.innerHTML = indexEsp["h2_n1"];
+        p_n1.innerHTML = indexEsp["p_n1"];
+        h2_n2.innerHTML = indexEsp["h2_n2"];
+        p_n2.innerHTML = indexEsp["p_n2"];
+    }
+}
+
+
+let lang = localStorage.getItem("lang");
+let espBtn = document.getElementById("esp_btn");
+let engBtn = document.getElementById("eng_btn");
+console.log(lang);
+changeLang();
+
+espBtn.addEventListener("click", ()=> {
+    lang = "esp";
+    console.log(lang);
+    localStorage.setItem("lang", lang);
+    console.log("Language: ", lang);
+    changeLang();
 })
 
-engBtn.addEventListener("click", ()=>{
-    location.reload();
+engBtn.addEventListener("click",()=>{
+    lang = "eng";
+    console.log(lang);
+    localStorage.setItem("lang", null);
+    console.log("Language: eng");
+    changeLang();
+
 })
