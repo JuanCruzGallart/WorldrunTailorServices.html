d7404c5775f36c4f5b2d635a5bff743fcc02633e
JuanCruzGallart <gallartjuancruz@gmail.com>
2023-09-02 23:14:33 -0300

    123

--- js/script.js
+++ js/script.js
@@ -8,6 +8,11 @@ const indexEsp = {
     p_n2:`Con más de 25 años de experiencia en el rubro, en Worldrun Tailor Services nos aseguramos de proveer las soluciones que su Negocio necesita.`
 };
 
+const servEsp = {
+    servH1: `Soluciones hechas a medida para su negocio`,
+    servSmall: `Optimizando las logísticas de su Negocio`,
+}
+
 const navEsp = {
     aHome: `Inicio`,
     aServices: `Servicios`,
@@ -17,12 +22,13 @@ const navEsp = {
 
 const botonesEsp = {
     getStarted: `Comenzar`,
-    learnMore: `Aprende Más`
+    learnMore: `Leer Más`
 };
 
 const footerEsp = {
     footerP: `Asesoría sobre Negocios Internacionales para la Economía Global. <br> Ante cualquier consulta o si desea adquirir nuestros servicios, llámenos.`,
-
+    officeH3: `Oficina <div class="underline"><span></span></div>`,
+    copyright: `© 2023 Worldrun Tailor Services.  Todos los derechos reservados.`
 };
 
 //Nav variables
@@ -38,6 +44,8 @@ console.log(learnMoreBtn);
 
 //Footer variables
 let pFooter = document.getElementById("footer_p");
+let officeH3 = document.getElementById("office_h3");
+let copyright = document.getElementById("copyright");
 
 //Index variables
 let h1Land = document.getElementById("index_landing_h1");
@@ -48,6 +56,9 @@ let p_n1 = document.getElementById("p_n1");
 let h2_n2 = document.getElementById("h2_n2");
 let p_n2 = document.getElementById("p_n2");
 
+//Serv Variables
+let servH1 = document.getElementById("serv_h1");
+let servSmall = document.getElementById("serv_small");
 
 
 function changeLang(){
@@ -56,16 +67,21 @@ function changeLang(){
         location.reload();
     }else if(lang == "esp"){
 
+        //Navigation Links
         for (let i = 0; i < aHome.length; i++) {
             aHome[i].innerHTML = navEsp["aHome"]
           };
-        aServices.innerHTML = navEsp["aServices"];
-        aContact.innerHTML = navEsp["aContact"];
-        aAbout.innerHTML = navEsp["aAbout"];
-        h1Land.innerHTML = indexEsp["h1"];
-        pLand.innerHTML = indexEsp["p"];
-        smallLand.innerHTML = indexEsp["small"];
-        
+        for (let i = 0; i < aServices.length; i++) {
+        aServices[i].innerHTML = navEsp["aServices"]
+        };
+        for (let i = 0; i < aContact.length; i++) {
+        aContact[i].innerHTML = navEsp["aContact"]
+        };
+        for (let i = 0; i < aAbout.length; i++) {
+        aAbout[i].innerHTML = navEsp["aAbout"]
+        };
+
+        //Btns
         for (let i = 0; i < getStartedBtn.length; i++) {
             getStartedBtn[i].innerHTML = botonesEsp["getStarted"];
           };
@@ -74,10 +90,23 @@ function changeLang(){
             learnMoreBtn[i].innerHTML = botonesEsp["learnMore"];
           };
 
+        //Footer
+        pFooter.innerHTML = footerEsp["footerP"];
+        officeH3.innerHTML = footerEsp["officeH3"];
+        copyright.innerHTML = footerEsp["copyright"];
+
+        //Index
+        h1Land.innerHTML = indexEsp["h1"];
+        pLand.innerHTML = indexEsp["p"];
+        smallLand.innerHTML = indexEsp["small"];
         h2_n1.innerHTML = indexEsp["h2_n1"];
         p_n1.innerHTML = indexEsp["p_n1"];
         h2_n2.innerHTML = indexEsp["h2_n2"];
         p_n2.innerHTML = indexEsp["p_n2"];
+
+        //Serv
+        servH1.innerHTML = servEsp["servH1"];
+        servSmall.innerHTML = servEsp["servSmall"];
     }
 }
 
